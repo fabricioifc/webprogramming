ca4b74088dc47bc2a4d3f83f5aa40b76048a7f0d
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2017-05-23 11:01:27 -0300

    commit

--- aula02/src/java/br/com/ifc/services/UsuarioService.java
+++ aula02/src/java/br/com/ifc/services/UsuarioService.java
@@ -14,11 +14,11 @@ import java.util.List;
  */
 public interface UsuarioService {
 
-    public List<Usuarios> listar() throws Exception;
+    // public List<Usuarios> listar() throws Exception;
     public boolean salvar(Usuarios usuario) throws Exception;
     public boolean atualizar(Usuarios usuario) throws Exception;
     public boolean deletar(Integer usuario) throws Exception;
-    public Usuarios getById(Integer id) throws Exception;
-    public Usuarios autenticar(String usuario, String senha) throws Exception;
-    public Usuarios getByUsuario(String usuario) throws Exception;
+    // public Usuarios getById(Integer id) throws Exception;
+    // public Usuarios autenticar(String usuario, String senha) throws Exception;
+    // public Usuarios getByUsuario(String usuario) throws Exception;
 }

f277d2e0a4c17eb606dffecdcfa452a6791e607e
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2017-05-31 15:22:03 -0300

    commit

--- ControleUsuario/web/resources/sql.sql
+++ ControleUsuario/web/resources/sql.sql
@@ -1,5 +1,5 @@
-drop database servlet_login_db;
-create database servlet_login_db;
+drop database if exists servlet_login_db;
+create database if not exists servlet_login_db;
 use servlet_login_db;
 
 CREATE TABLE usuarios (
@@ -19,8 +19,11 @@ CREATE TABLE filmes (
     PRIMARY KEY(id)
 );
 
+-- Insere usuários
+insert into usuarios (nome, email, usuario, senha) values ('admin', 'admin@admin.com.br', 'admin', 'admin');
+
+-- Insere Filmes
 insert into filmes (nome, genero, imagem) values ('Rei Arthur – A Lenda da Espada', 'Ação', 'https://gds-wifmtpphmjvvgffvmg.netdna-ssl.com/contentFiles/image/opt_w840h0/cinema/2017/janeiro-2017/arthur.jpg');
 insert into filmes (nome, genero, imagem) values ('Mulher-Maravilha', 'Qudrinhos', 'http://br.web.img2.acsta.net/videothumbnails/17/01/03/13/26/084343.jpg');
 insert into filmes (nome, genero, imagem) values ('Logan', 'Quadrinhos', 'http://s2.glbimg.com/gPIvxBGroxjtNnMovwnb5j3A0co=/620x0/top/s.glbimg.com/jo/eg/f/original/2016/10/20/logan3.jpg');
 insert into filmes (nome, genero, imagem) values ('Guardiões da Galáxia 2', 'Quadrinhos', 'http://rollingstone.uol.com.br/media/images/original/2014/07/27/img-1024882-guardioes-da-galaxia.jpg');
-insert into filmes (nome, genero, imagem) values ('Star Wars Rogue One', 'Ficção', 'http://blogs-images.forbes.com/erikkain/files/2016/11/rogue-one-rectangle.jpg');
